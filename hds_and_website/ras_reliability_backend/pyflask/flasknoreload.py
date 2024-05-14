0837376d14c4122e0d19b1d22677b65f99bd3adf
elias hartmark <elias.hartmark@gmail.com>
2024-05-14 12:23:00 +0200

    fixed error with use of datetime

--- hds_and_website/ras_reliability_backend/pyflask/flasknoreload.py
+++ hds_and_website/ras_reliability_backend/pyflask/flasknoreload.py
@@ -129,7 +129,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")  # Formatting datetime to a string
         message = f"handlerdtt_assumption violation detected: {format_time}"
         self.app.config['handlerdtt_assumption'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -141,7 +142,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handlerclassifier_assumption violation detected: {format_time}"
         self.app.config['handlerclassifier_assumption'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -153,7 +155,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handlerstate_req000 violation detected: {format_time}"
         self.app.config['handlerstate_req000'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -165,7 +168,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handleroperationalstate_0 violation detected: {format_time}"
         self.app.config['handleroperationalstate_0'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -177,7 +181,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handleroperationalstate_1 violation detected: {format_time}"
         self.app.config['handleroperationalstate_1'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -189,7 +194,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handleroperationalstate_2 violation detected: {format_time}"
         self.app.config['handleroperationalstate_2'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -201,7 +207,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handleroperationalstate_3 violation detected: {format_time}"
         self.app.config['handleroperationalstate_3'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -213,7 +220,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handlerstate_req101 violation detected: {format_time}"
         self.app.config['handlerstate_req101'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -225,7 +233,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handlerstate_req102 violation detected: {format_time}"
         self.app.config['handlerstate_req102'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -237,7 +246,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handlerstate_req103 violation detected: {format_time}"
         self.app.config['handlerstate_req103'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -249,7 +259,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handlerstate_req104 violation detected: {format_time}"
         self.app.config['handlerstate_req104'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -261,7 +272,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handlerstate_req201 violation detected: {format_time}"
         self.app.config['handlerstate_req201'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -273,7 +285,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handlerstate_req202 violation detected: {format_time}"
         self.app.config['handlerstate_req202'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
@@ -285,7 +298,8 @@ class TeleopNode(Node):
         format_time = current_time.strftime("%a, %d %b %Y %H: %M: %S GMT")
         message = f"handlerstate_req203 violation detected: {format_time}"
         self.app.config['handlerstate_req203'] = message
-        now = datetime.datetime.now()
+        
+        now = datetime.now()
         log_filename = '/home/eliash/overhead.log'
         formatted_time = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         with open(log_filename, 'a') as file:
